ce5f1423a42cdec7c4e46b98d97056dea8668ae1
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-03-26 14:56:31 +0300

    Refactor code to improve performance and readability

---
+++ pages/products/[id].vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>
+      {{ product.title }} and the id is <b>{{ product.id }}</b>
+    </p>
+    <span>{{ product.price }}</span>
+    <p>
+      {{ product.description }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+const url = "https://fakestoreapi.com/products/" + id;
+
+const { data: product } = await useFetch(url);
+
+definePageMeta({
+  layout: "products",
+});
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Products</p>
+
+  <div class="grid grid-cols-4 gap-4">
+    <div v-for="product in products" :key="product.id">
+      <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+
+<style></style>
